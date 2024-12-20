5cb973f995f6e867a7e21a241ec13b82805e8356
Fabricio Jorge <fabricioj.dev@gmail.com>
2024-12-20 15:20:18 -0400

    repositorio de aulas

---
+++ Estrutura de repetição/estruturaDeRepeticao.py
@@ -0,0 +1,12 @@
+# texto = input('informe um texto')
+# VOGAIS = 'AEIOU'
+
+# for letra in texto:
+#     if letra.upper() in VOGAIS:
+#         print(letra, end='')
+
+# print()
+
+# for numero in range(0, 200, 10):
+#     print(numero, end=' ')
+
---
+++ Estrutura de repetição/trabalhando_com_string/metodosUteis.py
@@ -0,0 +1,26 @@
+#METODOS STRING
+
+# nome = '   FabRiCiO   '
+
+# print(nome.upper())
+# print(nome.lower())
+# print(nome.title())
+# print(nome.strip())
+# print(nome.lstrip())
+# print(nome.rstrip())
+
+# texto = 'Fabricio'
+# print(texto.center(16, '#'))
+# print('-'.join(texto))
+
+
+#FATIAMENTO DE STRING
+
+# nome = 'Fabricio Jorge Souza Magalhães'
+
+# print(nome[2: 20])
+# print(nome[::-1])
+
+#STRING MULTIPLAS LINHAS
+
+
